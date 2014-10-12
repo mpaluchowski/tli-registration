e6e5a0271b3147d6ea2a1c7c872097eb7618f058
Michal Paluchowski <michal@paluchowski.com>
2014-10-12 08:06:33 +0200

    Updating jQuery to 2.x line

--- app/ui/main/index.php
+++ app/ui/main/index.php
@@ -143,7 +143,7 @@
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   </body>
