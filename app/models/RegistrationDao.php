ae7b9b390c46857fba9f85784282625049da0ac5
Michal Paluchowski <michal@paluchowski.com>
2014-10-14 12:31:45 +0200

    Configuring PDO to report errors via exceptions
    Otherwise we don't see them unless we check for them explicitly.

--- app/models/RegistrationDao.php
+++ app/models/RegistrationDao.php
@@ -8,7 +8,8 @@ class RegistrationDao {
 		\F3::set('db', new \DB\SQL(
 			'mysql:host=' . \F3::get('db_host') . ';port=' . \F3::get('db_port') . ';dbname=' . \F3::get('db_database'),
 			\F3::get('db_username'),
-			\F3::get('db_password')
+			\F3::get('db_password'),
+			[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
 		));
 	}
 
