feaea63afde81948afc235826f7b5202262fcee7
Michal Paluchowski <michal@paluchowski.com>
2014-10-15 19:06:32 +0200

    Preventing robots from indexing the page

--- app/ui/main/index.php
+++ app/ui/main/index.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="robots" content="noindex, nofollow, noarchive, noimageindex">
+
     <title><?php echo \F3::get('lang.RegistrationFormPageTitle') ?> | TLI 2015</title>
 
     <!-- Bootstrap -->
