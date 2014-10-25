4556a5c76c586033ed6b127c387cd64ce187e19c
Michal Paluchowski <michal@paluchowski.com>
2014-10-25 11:50:06 +0200

    Adding more test data

--- sql/data-test.sql
+++ sql/data-test.sql
@@ -2,6 +2,14 @@
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Dumping data for table tli_registrations.tli_administrators: ~1 rows (approximately)
+DELETE FROM `tli_administrators`;
+/*!40000 ALTER TABLE `tli_administrators` DISABLE KEYS */;
+INSERT INTO `tli_administrators` (`id_administrator`, `full_name`, `email`, `password`) VALUES
+	(1, 'Michał Paluchowski', '***REMOVED***', '$2y$10$ZDTlEI.ER1GXb4YvTuZ/E.vjV9hwZCGI1su2Gd8qtXek2loHcpGK6');
+/*!40000 ALTER TABLE `tli_administrators` ENABLE KEYS */;
+
+-- Dumping data for table tli_registrations.tli_clubs: ~4 rows (approximately)
 DELETE FROM `tli_clubs`;
 /*!40000 ALTER TABLE `tli_clubs` DISABLE KEYS */;
 INSERT INTO `tli_clubs` (`id_club`, `name`) VALUES
@@ -9,6 +17,45 @@ INSERT INTO `tli_clubs` (`id_club`, `name`) VALUES
 	(2, 'Toastmasters Polska SA'),
 	(3, 'Top Careers Toastmasters');
 /*!40000 ALTER TABLE `tli_clubs` ENABLE KEYS */;
+
+-- Dumping data for table tli_registrations.tli_registrations: ~4 rows (approximately)
+DELETE FROM `tli_registrations`;
+/*!40000 ALTER TABLE `tli_registrations` DISABLE KEYS */;
+INSERT INTO `tli_registrations` (`id_registration`, `email`, `hash`, `date_entered`, `date_paid`) VALUES
+	(1, '***REMOVED***', '30085d747a6d6e2711c7c2446fdf296dc65d67f5', '2014-10-16 09:50:26', NULL),
+	(2, 'john@example.com', '5224cb6fdd5bbe463af1db8ee499e858fcb79f81', '2014-10-16 10:50:42', NULL),
+	(3, 'jane@example.com', '0850a4cffb73cbc53fd33e8990c2184c915ff041', '2014-10-19 22:46:36', '2014-10-19 22:46:56');
+/*!40000 ALTER TABLE `tli_registrations` ENABLE KEYS */;
+
+-- Dumping data for table tli_registrations.tli_registration_fields: ~29 rows (approximately)
+DELETE FROM `tli_registration_fields`;
+/*!40000 ALTER TABLE `tli_registration_fields` DISABLE KEYS */;
+INSERT INTO `tli_registration_fields` (`fk_registration`, `name`, `value`) VALUES
+	(1, 'accommodation-on', '["fri-sat"]'),
+	(1, 'accommodation-with-toastmasters', '"stay"'),
+	(1, 'comments', '"This is my comment. I\'ll add some  code to that."'),
+	(1, 'educational-awards', '"ACS, ALB"'),
+	(1, 'exec-position', '"vpe"'),
+	(1, 'friday-social-event', '"on"'),
+	(1, 'full-name', '"Michal Paluchowski"'),
+	(1, 'home-club', '"Speaking Elephants"'),
+	(1, 'phone', '"+48888205402"'),
+	(1, 'saturday-dinner-meal', '"vegetarian"'),
+	(1, 'saturday-dinner-participate', '"on"'),
+	(1, 'saturday-party-participate', '"on"'),
+	(1, 'sleep-on', '["mattress","foam-pad"]'),
+	(2, 'accommodation-with-toastmasters', '"host"'),
+	(2, 'exec-position', '"none"'),
+	(2, 'friday-social-event', '"on"'),
+	(2, 'full-name', '"John Doe"'),
+	(2, 'home-club', '"None"'),
+	(2, 'phone', '"+48694470100"'),
+	(3, 'accommodation-with-toastmasters', '"independent"'),
+	(3, 'exec-position', '"vpe"'),
+	(3, 'full-name', '"Jane Doe"'),
+	(3, 'home-club', '"Speaking Elephants"'),
+	(3, 'phone', '"+48 123 456 789"');
+/*!40000 ALTER TABLE `tli_registration_fields` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
