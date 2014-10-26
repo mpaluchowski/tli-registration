46ca25e81153da87dce05343ecf0a9d6aae7d49d
Michal Paluchowski <michal@paluchowski.com>
2014-10-26 19:49:50 +0100

    Initial data set
    Includes a list of all current Polish Toastmasters clubs.

---
+++ sql/data-init.sql
@@ -0,0 +1,54 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Dumping data for table tli_registrations.tli_clubs: ~4 rows (approximately)
+DELETE FROM `tli_clubs`;
+/*!40000 ALTER TABLE `tli_clubs` DISABLE KEYS */;
+INSERT INTO `tli_clubs` (`id_club`, `name`) VALUES
+	(38, 'ASBIRO Toastmasters'),
+	(11, 'BBH Poland Toastmasters'),
+	(20, 'Bydgoszcz Toastmasters Professionals'),
+	(18, 'Grudziądz Toastmasters'),
+	(8, 'Krakow Public Speaking Club'),
+	(30, 'Lodz Toastmasters'),
+	(10, 'Pionier Rzeszów Toastmasters'),
+	(15, 'Profesjonalni Mówcy i Liderzy'),
+	(27, 'Sedina Toastmasters'),
+	(33, 'Shall We Speak?'),
+	(6, 'Silesia Speech Masters'),
+	(4, 'Silesia Toastmasters'),
+	(16, 'Sokrates Toruń'),
+	(1, 'Speaking Elephants'),
+	(12, 'State Street Kraków'),
+	(31, 'The Leader Ship'),
+	(41, 'Toast @ Capgemini'),
+	(37, 'Toastmasters @ EDC'),
+	(13, 'Toastmasters AGH'),
+	(36, 'Toastmasters Białystok'),
+	(17, 'Toastmasters Bydgoszcz'),
+	(5, 'Toastmasters Częstochowa'),
+	(22, 'Toastmasters Gdańsk'),
+	(25, 'Toastmasters Gdynia'),
+	(9, 'Toastmasters Kraków'),
+	(35, 'Toastmasters Leaders'),
+	(34, 'Toastmasters Lublin'),
+	(7, 'Toastmasters Na Szczycie'),
+	(24, 'Toastmasters Olsztyn'),
+	(14, 'Toastmasters PK'),
+	(2, 'Toastmasters Polska SA'),
+	(28, 'Toastmasters Poznań'),
+	(23, 'Toastmasters Sopot Leaders'),
+	(26, 'Toastmasters Szczecin Passionate Speakers'),
+	(32, 'Toastmasters w Orange'),
+	(3, 'Top Careers Toastmasters Club'),
+	(19, 'Toruń Toastmasters'),
+	(21, 'Tricity Toastmasters'),
+	(29, 'Verbal Victory'),
+	(39, 'Vistula Toastmasters Leaders'),
+	(42, 'WrocLove Speakers'),
+	(40, 'WrocLove Toastmasters PL');
+/*!40000 ALTER TABLE `tli_clubs` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
