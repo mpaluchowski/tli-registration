68895b270d34729769d6dae2720d89a48ff866e3
Michal Paluchowski <michal@paluchowski.com>
2014-10-26 07:31:46 +0100

    Prefixing scope to avoid collisions

--- app/models/FlashScope.php
+++ app/models/FlashScope.php
@@ -5,17 +5,17 @@ namespace models;
 class FlashScope {
 
 	static function push($key, $value) {
-		\F3::set('SESSION.' . $key, $value);
+		\F3::set('SESSION.flash.' . $key, $value);
 	}
 
 	static function pop($key) {
-		$value = \F3::get('SESSION.' . $key);
-		\F3::clear('SESSION.' . $key);
+		$value = \F3::get('SESSION.flash.' . $key);
+		\F3::clear('SESSION.flash.' . $key);
 		return $value;
 	}
 
 	static function has($key) {
-		return \F3::get('SESSION.' . $key);
+		return (bool)\F3::get('SESSION.flash.' . $key);
 	}
 
 }
