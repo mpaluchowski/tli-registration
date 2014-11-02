e26194dd1107883b5c3a47bb351fe905686543e8
Michal Paluchowski <michal@paluchowski.com>
2014-11-02 08:04:47 +0100

    Adding comments

--- app/models/PriceCalculatorFactory.php
+++ app/models/PriceCalculatorFactory.php
@@ -2,8 +2,18 @@
 
 namespace models;
 
+/**
+ * Provides instances of \models\PriceCalculator implementations.
+ */
 abstract class PriceCalculatorFactory {
 
+	/**
+	 * Return a new instance of a PriceCalculator implementation.
+	 *
+	 * @return new instance of what's configured as the PriceCalculator
+	 * implementation.
+	 * @throws Exception when form_price_calculator configuration string missing.
+	 */
 	static function newInstance() {
 		if (!\F3::get('form_price_calculator'))
 			throw new \Exception('form_price_calculator configuration cannot be empty. Enter the full classpath of the \models\PriceCalculator implementation.');
