4cf911a1d6b3a9438b57401065c4da21128409bd
Michal Paluchowski <michal@paluchowski.com>
2014-11-09 11:20:53 +0100

    Adding comments to View helper

--- app/helpers/View.php
+++ app/helpers/View.php
@@ -2,14 +2,27 @@
 
 namespace helpers;
 
+/**
+ * Helping methods useful for the views.
+ */
 class View {
 
+	/**
+	 * @return base URL of the application, the hostname.
+	 */
 	static function getBaseUrl() {
 		return \F3::get('SCHEME')
 			. '://'
 			. $_SERVER['SERVER_NAME'];
 	}
 
+	/**
+	 * Produces the label type to display for each registration status.
+	 *
+	 * @param status Registration status to provide a label for.
+	 * @return class name to use for the label for given status.
+	 * @throws exception when status provided isn't supported.
+	 */
 	static function getRegistrationStatusLabelClass($status) {
 		switch ($status) {
 			case 'PENDING_PAYMENT':
