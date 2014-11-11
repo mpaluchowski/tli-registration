3f6e55542acabb85db2db84b65272e03afe51af3
Michal Paluchowski <michal@paluchowski.com>
2014-11-11 08:59:21 +0100

    Cleaning buffer when displaying error

--- app/controllers/Error.php
+++ app/controllers/Error.php
@@ -5,6 +5,10 @@ namespace controllers;
 class Error {
 
 	function handle($f3) {
+		// Clean output buffers if error happened mid-page
+		while (ob_get_level())
+			ob_end_clean();
+
 		if ('404' == $f3->get('ERROR.code')) {
 			echo \View::instance()->render('error/404.php');
 		} else {
