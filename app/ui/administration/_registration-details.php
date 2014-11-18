4eb2c520e0c83d5b3b2651e6ade701a3dcdf27e9
Michal Paluchowski <michal@paluchowski.com>
2014-11-18 21:04:52 +0100

    Using FormRenderer in registration details view

--- app/ui/administration/_registration-details.php
+++ app/ui/administration/_registration-details.php
@@ -1,3 +1,7 @@
+<?php
+$renderer = \helpers\FormRendererFactory::className();
+?>
+
 <div class="container-fluid">
 <div class="row">
 	<div class="col-sm-6">
@@ -10,19 +14,19 @@
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.Country') ?></th>
-					<td><?php echo \F3::get('lang.Country-' . $form->getField('country')) ?></td>
+					<td><?php echo $renderer::value($form, 'country') ?></td>
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.HomeClub') ?></th>
-					<td><?php echo $form->getField('home-club') ?></td>
+					<td><?php echo $renderer::value($form, 'home-club') ?></td>
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.ExecCommmitteePosition') ?></th>
-					<td><?php echo \F3::get('lang.ExecCommmitteePosition-' . $form->getField('exec-position')) ?></td>
+					<td><?php echo $renderer::value($form, 'exec-position') ?></td>
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.EducationalAwards') ?></th>
-					<td><?php echo $form->hasField('educational-awards') ? $form->getField('educational-awards') : \F3::get('lang.EducationalAwardsNone') ?></td>
+					<td><?php echo $renderer::value($form, 'educational-awards') ?></td>
 				</tr>
 				<tr>
 					<td colspan="2">
@@ -31,14 +35,12 @@
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.AccommodationWithToastmasters') ?></th>
-					<td><?php echo \F3::get('lang.AccommodationWithToastmasters-' . $form->getField('accommodation-with-toastmasters')) ?></td>
+					<td><?php echo $renderer::value($form, 'accommodation-with-toastmasters') ?></td>
 				</tr>
-			<?php if ($form->hasField('accommodation-on')):
-				$accommodationOnOptions = array_map(function ($item) { return \F3::get('lang.AccommodationWithToastmasters-' . $item); }, $form->getField('accommodation-on'));
-			?>
+			<?php if ($form->hasField('accommodation-on')): ?>
 				<tr>
 					<th><?php echo \F3::get('lang.AccommodationWithToastmastersNeededOn') ?></th>
-					<td><?php echo implode(", ", $accommodationOnOptions) ?></td>
+					<td><?php echo $renderer::value($form, 'accommodation-on') ?></td>
 				</tr>
 			<?php endif; ?>
 			</tbody>
@@ -55,37 +57,31 @@
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.EventsTranslator') ?></th>
-					<td><?php echo $form->hasField('translator') && "on" === $form->getField('translator') ? \F3::get('lang.Yes') : \F3::get('lang.No') ?></td>
+					<td><?php echo $renderer::value($form, 'translator') ?></td>
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.EventsContest') ?></th>
-					<td><?php echo $form->hasField('contest-attend') && "on" === $form->getField('contest-attend') ? \F3::get('lang.Yes') : \F3::get('lang.No') ?></td>
+					<td><?php echo $renderer::value($form, 'contest-attend') ?></td>
 				</tr>
-			<?php if ($form->hasField('friday-copernicus-options')) {
-				$copernicusOptions = array_map(function ($item) { return \F3::get('lang.EventsFridayCopernicusAttend-' . $item); }, $form->getField('friday-copernicus-options'));
-			} ?>
 				<tr>
 					<th><?php echo \F3::get('lang.EventsFridayCopernicus') ?></th>
-					<td><?php echo $form->hasField('friday-copernicus-attend') && "on" === $form->getField('friday-copernicus-attend') ? \F3::get('lang.Yes') . ', ' . implode(", ", $copernicusOptions) : \F3::get('lang.No') ?></td>
+					<td><?php echo $renderer::value($form, 'friday-copernicus-attend') ?></td>
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.EventsFridaySocial') ?></th>
-					<td><?php echo $form->hasField('friday-social-event') && "on" === $form->getField('friday-social-event') ? \F3::get('lang.Yes') : \F3::get('lang.No') ?></td>
+					<td><?php echo $renderer::value($form, 'friday-social-event') ?></td>
 				</tr>
-			<?php if ($form->hasField('lunch-days')) {
-				$lunchDaysOptions = array_map(function ($item) { return \F3::get('lang.EventsLunch-' . $item); }, $form->getField('lunch-days'));
-			} ?>
 				<tr>
 					<th><?php echo \F3::get('lang.EventsLunch') ?></th>
-					<td><?php echo $form->hasField('lunch') && "on" === $form->getField('lunch') ? \F3::get('lang.Yes') . ', ' . implode(", ", $lunchDaysOptions) : \F3::get('lang.No') ?></td>
+					<td><?php echo $renderer::value($form, 'lunch') ?></td>
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.EventsSaturdayDinner') ?></th>
-					<td><?php echo $form->hasField('saturday-dinner-participate') && "on" === $form->getField('saturday-dinner-participate') ? \F3::get('lang.Yes') . ', ' . $form->getField('saturday-dinner-meal') : \F3::get('lang.No') ?></td>
+					<td><?php echo $renderer::value($form, 'saturday-dinner-participate') ?></td>
 				</tr>
 				<tr>
 					<th><?php echo \F3::get('lang.EventsSaturdayParty') ?></th>
-					<td><?php echo $form->hasField('saturday-party-participate') && "on" === $form->getField('saturday-party-participate') ? \F3::get('lang.Yes') : \F3::get('lang.No') ?></td>
+					<td><?php echo $renderer::value($form, 'saturday-party-participate') ?></td>
 				</tr>
 			</tbody>
 		</table>
@@ -96,7 +92,7 @@
 	<div class="col-sm-12">
 		<h4><?php echo \F3::get('lang.CommentsHeader') ?></h4>
 
-		<p><?php echo $form->hasField('comments') ? nl2br($form->getField('comments'), false) : \F3::get('lang.CommentsNone') ?></p>
+		<p><?php echo $renderer::value($form, 'comments') ?></p>
 	</div>
 </div>
 </div>
