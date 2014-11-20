3735e827f65a943d656cb2ea7633ba24b5cb451c
Michal Paluchowski <michal@paluchowski.com>
2014-11-20 17:11:34 +0100

    Styling to match basic TLI website

--- app/ui/header.php
+++ app/ui/header.php
@@ -11,6 +11,7 @@
 
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700&amp;subset=latin,latin-ext'>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/screen.css">
 
