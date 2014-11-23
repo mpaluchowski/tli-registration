ad28ccf3afb7d48e48f9a4ed0de3b17fcf507ec4
Michal Paluchowski <michal@paluchowski.com>
2014-11-23 12:39:13 +0100

    Allow fetching class name of configured PaymentProcessor

--- app/models/PaymentProcessorFactory.php
+++ app/models/PaymentProcessorFactory.php
@@ -8,16 +8,32 @@ namespace models;
 class PaymentProcessorFactory {
 
 	/**
+	 * @return class name of the PaymentProcessor implementation
+	 * @throws exception when no PaymentProcessor implementation is configured
+	 */
+	static function className() {
+		self::checkConfig();
+		return \F3::get('payment_processor');
+	}
+
+	/**
 	 * @return instance of a \models\PaymentProcessor implementation configured
 	 * for use via config.ini.
 	 * @throws exception when no PaymentProcessor implementation is configured.
 	 */
 	static function instance() {
-		if (!\F3::get('payment_processor'))
-			throw new \Exception("payment_processor configuration cannot be empty. Enter the full classpath of the \models\PaymentProcessor implementation.");
-
+		self::checkConfig();
 		$name = \F3::get('payment_processor');
 		return new $name;
+	}
+
+	/**
+	 * Check if implementation of PaymentProcessor is correctly configured.
+	 * Throw exception if not.
+	 */
+	protected static function checkConfig() {
+		if (!\F3::get('payment_processor'))
+			throw new \Exception("payment_processor configuration cannot be empty. Enter the full classpath of the \models\PaymentProcessor implementation.");
 	}
 
 }
