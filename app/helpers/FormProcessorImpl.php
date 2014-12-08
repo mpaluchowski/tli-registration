2ec14be1d834e1cb9ee48609f0e05857c43d9dc7
Michal Paluchowski <michal@paluchowski.com>
2014-12-08 21:09:51 +0100

    Normalize club names, in addition to full names

--- app/helpers/FormProcessorImpl.php
+++ app/helpers/FormProcessorImpl.php
@@ -12,10 +12,7 @@ class FormProcessorImpl implements \helpers\FormProcessor {
 		// like ALL CAPS strings.
 		$form->setField(
 			'full-name',
-			mb_convert_case(
-				mb_strtolower($form->getField('full-name')),
-				MB_CASE_TITLE
-				)
+			self::normalizeName($form->getField('full-name'))
 			);
 
 		// Registrations from outside Poland and Toastmasters are put on hold
@@ -28,10 +25,13 @@ class FormProcessorImpl implements \helpers\FormProcessor {
 		// Check if a custom club name was entered
 		if ($form->hasField('home-club-custom')
 				&& $form->getField('home-club-custom')) {
-			$dictionaryDao = new \models\DictionaryDao();
-			$dictionaryDao->createClub($form->getField('home-club-custom'));
 
-			$form->setField('home-club', $form->getField('home-club-custom'));
+			$club = self::normalizeName($form->getField('home-club-custom'));
+
+			$dictionaryDao = new \models\DictionaryDao();
+			$dictionaryDao->createClub($club);
+
+			$form->setField('home-club', $club);
 			$form->clearField('home-club-custom');
 		}
 
@@ -39,6 +39,17 @@ class FormProcessorImpl implements \helpers\FormProcessor {
 		if ($form->hasField('data-collection-consent')) {
 			$form->clearField('data-collection-consent');
 		}
+	}
+
+	/**
+	 * Normalizes the case of a name, by capitalizing the first letter of every
+	 * word and leaving the rest lowercase.
+	 *
+	 * @param $name the name to normalize
+	 * @return the name normalized
+	 */
+	static function normalizeName($name) {
+		return mb_convert_case(mb_strtolower($name), MB_CASE_TITLE);
 	}
 
 }
