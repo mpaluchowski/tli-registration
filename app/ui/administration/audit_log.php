cb01987774b3190f17c92364a6e178757d9734f3
Michal Paluchowski <michal@paluchowski.com>
2014-12-21 19:33:03 +0100

    Quick fix to audit log wrapping

--- app/ui/administration/audit_log.php
+++ app/ui/administration/audit_log.php
@@ -19,14 +19,14 @@
 		<tbody>
 		<?php foreach ($events as $event): ?>
 			<tr data-id="<?php echo $event->id ?>">
-				<td>
+				<td class="text-nowrap">
 					<img src="<?php echo \helpers\View::getGravatarUrl($event->administratorEmail, 30) ?>" alt="<?php echo $event->administratorName ?>">
 					<?php echo $event->administratorName ?>
 				</td>
 				<td><?php echo $event->name ?></td>
 				<td><?php echo $event->objectName ? $event->objectName . '(' . $event->objectId . ')' : "&mdash;" ?></td>
 				<td><?php echo $event->data ?: "&mdash;" ?></td>
-				<td><?php echo \helpers\View::formatDateTime($event->dateOccurred) ?></td>
+				<td class="text-nowrap"><?php echo \helpers\View::formatDateTime($event->dateOccurred) ?></td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
