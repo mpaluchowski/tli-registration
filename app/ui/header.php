dd000ecc0e4b28e7f05747f27db737d2d44a5ebf
Michal Paluchowski <michal@paluchowski.com>
2014-12-29 12:33:32 +0100

    Upgrading to jQuery 2.1.3

--- app/ui/header.php
+++ app/ui/header.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/screen.css">
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="/js/jquery.ajaxQueue.min.js"></script>
     <script type="text/javascript">
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
